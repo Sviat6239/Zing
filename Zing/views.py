0fd729a57a84ba27f7c3e12ce6009dd0c336e82f
Sviatoslav6239 <svf121209@gmail.com>
2024-11-29 14:51:53 +0100

    first commit

---
+++ Zing/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def homepage(request):
+    pass
+
+def aboutpage(request):
+    pass
+
+def registerpage(request):
+    pass
+
+def loginpage(request):
+    pass
+
+def logoutpage(request):
+    pass
+
+def profilepage(request):
+    pass
+
+def editprofilepage(request):
+    pass
+
+def viewprofilepage(request):
+    pass
+
+def dashboardpage(request):
+    pass
+
+def resetpasswordpage(request):
+    pass
+
+def supportpage(request):
+    pass
+
+def settingspage(request):
+    pass
